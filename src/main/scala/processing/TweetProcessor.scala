7ac9fe7b91fbfd88d5910c9da671952802b036b5
raghad zaidan <s12112557@stu.najah.edu>
2024-12-26 17:58:25 +0200

    add code for /streemig (TweetProducer.scala ,TweetConsumer.scala , KafkaConfig.scala)

--- src/main/scala/processing/TweetProcessor.scala
+++ src/main/scala/processing/TweetProcessor.scala
@@ -1,2 +1,13 @@
 package processing
 
+object TweetProcessor {
+  def process(tweet: String): Unit = {
+    println(s"Processing tweet: $tweet")
+
+    // Example: Extract hashtags
+
+    // Example: Perform sentiment analysis
+
+    // Add logic to save data to a database, if necessary
+  }
+}
