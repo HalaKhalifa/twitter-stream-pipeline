04751c4a07b8d713b030a2d29f269d5a052fe37b
Hala Khalifa <s12112858@stu.najah.edu>
2024-12-27 04:03:44 +0200

    Add the Configuration of Elasticsearch settings and the logic of Elasticsearch connection and indexing

--- src/main/scala/storage/ElasticsearchClient.scala
+++ src/main/scala/storage/ElasticsearchClient.scala
@@ -1,2 +1,32 @@
 package storage
 
+import org.apache.http.HttpHost
+import org.elasticsearch.client.{RestClient, RestHighLevelClient, RequestOptions}
+import org.elasticsearch.client.indices.CreateIndexRequest
+import org.elasticsearch.xcontent.XContentType
+
+object ElasticsearchClient {
+
+  // Create a connection to Elasticsearch using the configuration
+  def createClient(): RestHighLevelClient = {
+    new RestHighLevelClient(
+      RestClient.builder(
+        new HttpHost(ElasticsearchConfig.host, ElasticsearchConfig.port, ElasticsearchConfig.scheme)
+      )
+    )
+  }
+
+  // Create an index in Elasticsearch using a predefined schema
+  def createIndex(client: RestHighLevelClient, schema: String): Unit = {
+    val request = new CreateIndexRequest(ElasticsearchConfig.getIndexName)
+    request.source(schema, XContentType.JSON)
+    client.indices().create(request, RequestOptions.DEFAULT)
+    println(s"Index '${ElasticsearchConfig.getIndexName}' created successfully.")
+  }
+
+  // Close the client connection
+  def closeClient(client: RestHighLevelClient): Unit = {
+    client.close()
+    println("Elasticsearch client closed.")
+  }
+}
